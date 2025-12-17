2c2567cbfa953a2f6552c4c2bb3eed528dc82fc6
A123ditya <145685656+A123ditya@users.noreply.github.com>
2025-12-17 14:41:31 +0000

    con

---
+++ .devcontainer/basic/1datatype.js
@@ -0,0 +1,24 @@
+"use strict";// use the js are newer version , automatically use in strict mode 
+//alert(3+3) WR ARE USING NODE JS NOT BROWESER 
+console.log(3+3)//code readability should be high 
+
+console.log("aditya")
+
+let name="aditya"
+let age ="21"
+let isLoggedIN=false
+let state; 
+
+//number 
+//bigint 
+//strin
+//null---> standalone empty value 
+//undefined didnt define value 
+//symbol  ---> unique 
+//object 
+console.log(typeof"aditya");
+console.log(typeof age );
+console.log(typeof null);//object
+console.log(typeof undefined);
+
+
---
+++ .devcontainer/basic/variable.js
@@ -0,0 +1,17 @@
+const accountid = 14441
+let accountEmail= " bhamareaditya215@gmail.com"
+var accountpass=" radhamadan"
+accountCity="nashilk"
+let accountstate;
+
+// accountid = 53  not allowed 
+accountEmail="hc@hmail"
+accountpass="3767623"
+accountCity="jaipr"
+console.log(accountid);
+console.log(accountEmail);
+console.log([accountEmail,accountid,accountCity,accountpass,accountstate]);
+
+// not not not didnt use var , beacuse if issue in block code and funtional scope
+
+
